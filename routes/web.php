cb2b29b63ebeab023d40e7e78692692470a0d99c
Raskapcr <raska23si@mahasiswa.pcr.ac.id>
2026-04-26 15:33:04 +0700

    baru

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+// Import Controllers
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BengkelController;
 use App\Http\Controllers\Controller2;
@@ -10,20 +12,20 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\MitraController;
 use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\PegawaiController; // Saya tambahkan ini agar tidak error
+use App\Http\Controllers\PegawaiController;
 
-// 1. Halaman utama sekarang langsung ke Login
+// 1. Landing Page (Root)
 Route::get('/', [AuthController::class, 'showLogin'])->name('hal-login');
 
-// Route Auth lainnya (Tetap ada atau bisa dihapus jika ingin pakai / saja)
-Route::get('/auth', [AuthController::class, 'showLogin']);
+// 2. Auth Routes
+Route::get('/auth', [AuthController::class, 'showLogin'])->name('auth.show');
 Route::post('/auth/login', [AuthController::class, 'login'])->name('login');
 Route::get('/auth/forgot', [AuthController::class, 'show_forgot_password'])->name('auth.forgot');
 Route::get('/auth/logout', [AuthController::class, 'logout'])->name('logout');
 Route::get('/auth/redirect-google', [AuthController::class, 'redirectToGoogle'])->name('redirect.google');
 Route::get('/oauthcallback', [AuthController::class, 'handleGoogleCallback']);
 
-// --- Route Info & Testing ---
+// 3. Testing & Helper Routes
 Route::get('/now', function () {
     return 'Tanggal hari ini:' . now('Asia/Jakarta')->format('d-m-Y H:i:s');
 });
@@ -33,30 +35,25 @@ Route::get('/info', function () {
 });
 
 Route::get('/calculate/{param1}/{param2}', function ($param1, $param2) {
-    if ($param1 == 0 && $param2 == 0) {
-        return "Silahkan masuk parameter";
-    } elseif ($param1 == 0) {
-        return $param2;
-    } elseif ($param2 == 0) {
-        return $param1;
-    } else {
-        return $param1 * $param2;
-    }
+    if ($param1 == 0 && $param2 == 0) return "Silahkan masuk parameter";
+    if ($param1 == 0) return $param2;
+    if ($param2 == 0) return $param1;
+    return $param1 * $param2;
 });
 
-// --- Route Controller 2 ---
+// 4. Controller 2 Routes
 Route::get('/lulu', [Controller2::class, 'welcome']);
 Route::get('/harini', [Controller2::class, 'currentTime']);
 Route::get('/hitung/{param1}/{param2}', [Controller2::class, 'handleParams']);
 
-// --- Route Pegawai ---
+// 5. Pegawai Routes
 Route::get('/pegawai/index', [PegawaiController::class, 'index']);
 Route::get('/pegawai/store/{param1}/{param2}', [PegawaiController::class, 'store']);
 Route::get('/pegawai/show/{id}', [PegawaiController::class, 'show']);
 Route::get('/pegawai/edit/{param1}', [PegawaiController::class, 'edit']);
 Route::get('/pegawai/destroy/{param1}', [PegawaiController::class, 'destroy']);
 
-// --- Route Mitra ---
+// 6. Mitra Routes
 Route::get('mitra', [MitraController::class, 'index'])->name('mitra.list');
 Route::get('mitra/create', [MitraController::class, 'create'])->name('mitra.create');
 Route::post('mitra/store', [MitraController::class, 'store'])->name('mitra.store');
@@ -64,7 +61,7 @@ Route::get('mitra/edit/{param1}', [MitraController::class, 'edit'])->name('mitra
 Route::post('mitra/update', [MitraController::class, 'update'])->name('mitra.update');
 Route::get('mitra/destroy/{param1}', [MitraController::class, 'destroy'])->name('mitra.destroy');
 
-// --- Route Group dengan Middleware ---
+// 7. Protected Routes (Membutuhkan Login)
 Route::group(['middleware' => ['checkislogin']], function () {
 
     Route::get('pelanggan', [PelangganController::class, 'index'])->name('pelanggan.list');
@@ -91,13 +88,13 @@ Route::group(['middleware' => ['checkislogin']], function () {
     Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
 });
 
-// --- Route Home/Landing Page ---
+// 8. Public Pages
 Route::get('home', [HomeController::class, 'index'])->name('home');
 Route::get('service', [HomeController::class, 'show_service'])->name('service');
 Route::get('sparepart', [HomeController::class, 'show_sparepart'])->name('sparepart');
 Route::get('about', [HomeController::class, 'show_about'])->name('about');
 
-// --- Route Registrasi Bengkel ---
+// 9. Registrasi Bengkel
 Route::get('/registrasi', [BengkelController::class, 'showRegister'])->name('register.bengkel');
 Route::post('/register', [BengkelController::class, 'register'])->name('register');
 Route::post('bengkel/store', [BengkelController::class, 'storeRegis'])->name('storeRegis');
